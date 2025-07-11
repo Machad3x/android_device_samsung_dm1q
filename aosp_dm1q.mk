4e9bf5af431054a145f5c6741a2fba836f6452d0
Machad3x <machad3x@gmail.com>
2025-07-11 12:34:52 -0400

    dm1q: Welcome to Matrixx

--- aosp_dm1q.mk
+++ aosp_dm1q.mk
@@ -11,10 +11,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from the device configuration.
 $(call inherit-product, device/samsung/dm1q/device.mk)
 
-# Inherit from the AOSP configuration.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit from the LineageOS configuration.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-PRODUCT_NAME := aosp_dm1q
+PRODUCT_NAME := lineage_dm1q
 PRODUCT_DEVICE := dm1q
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-S911B
@@ -27,3 +27,11 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildFingerprint=samsung/dm1qxxx/dm1q:15/AP3A.240905.015.A2/S911BXXS8DYF1:user/release-keys \
     DeviceProduct=dm1qxxx \
     SystemName=dm1qxxx
+
+# Matrixx Flags
+WITH_GMS := true
+WITH_GMS_COMMS_SUITE := true
+TARGET_SUPPORTS_WALLEFFECT := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_INCLUDE_STOCK_AICORE := true
